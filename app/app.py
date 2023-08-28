1436a8f534da001ac221d1bb164415f916a6c621
liamlivingston <liamklivingston@gmail.com>
2023-08-28 13:36:17 -0700

    modified:   app/app.py 	new file:   app/static/36v_battery.jpg 	new file:   app/static/ebike_v3.jpg 	modified:   app/static/modem_battery.jpg 	modified:   app/templates/projects.html

--- app/app.py
+++ app/app.py
@@ -14,6 +14,9 @@ def projects():
 def ebike_v1():
     return render_template("ebike_v1.html", home="/")
 
+@app.route('/projects/batteries')
+def batteries():
+    return render_template("batteries.html", home="/")
 
 
 @app.route('/github')
