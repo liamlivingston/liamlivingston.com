a4c9e50ef3fc80f14c65f627c7c2285119ccd794
liamlivingston <liamklivingston@gmail.com>
2023-09-02 01:07:17 -0700

    added v3 page

--- app/app.py
+++ app/app.py
@@ -18,6 +18,10 @@ def ebike_v1():
 def ebike_v2():
     return render_template("ebike_v2.html", home="/")
 
+@app.route('/projects/ebike_v3')
+def ebike_v3():
+    return render_template("ebike_v3.html", home="/")
+
 @app.route('/projects/batteries')
 def batteries():
     return render_template("batteries.html", home="/")
