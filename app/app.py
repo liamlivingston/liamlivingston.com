341b845f254e5117b4455dda5b927f7760c337b8
liamlivingston <liamklivingston@gmail.com>
2024-05-18 14:43:59 -0700

    modified:   app/app.py

--- app/app.py
+++ app/app.py
@@ -77,4 +77,4 @@ def test():
     return render_template("test.html", home="/")
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port='80')
