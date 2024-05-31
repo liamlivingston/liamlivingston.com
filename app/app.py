38750db0933e5eb0e6a18a01ee4aaadc67d7867a
liam <liamklivingston@gmail.com>
2024-05-31 11:46:04 -0700

    modified:   app/app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,10 @@
-from flask import Flask, session, abort, redirect, request, render_template
+import os
+from flask import Flask, session, abort, redirect, request, render_template, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/home/liam/Downloads/uploads'
+ALLOWED_EXTENSIONS = {'zip', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 
 app = Flask("liamlivingston")
 
@@ -72,6 +78,39 @@ def solar_car():
     
     pass
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect("/upload")
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
 @app.route('/github')
 def github():
     return redirect("https://github.com/liamlivingston")
@@ -85,4 +124,5 @@ def test():
     return render_template("test.html", home="/")
 
 if __name__ == "__main__":
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     app.run(debug=True, host='0.0.0.0', port='80')
